d36db9dd57e49b0c0aa31a0c769422721ee7e60f
Charlesjhoh <charles.j.h.oh@gmail.com>
2026-05-12 18:24:22 +0900

    fix login test

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -9,29 +9,29 @@ export default function LoginPage() {
   const router = useRouter();
 
   useEffect(() => {
-    const checkSession = async () => {
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
+            const checkSession = async () => {
+          const {
+            data: { session },
+          } = await supabase.auth.getSession();
 
-  if (session?.user) {
-    const userId = session.user.id;
+          if (session?.user) {
+            const userId = session.user.id;
 
-    const { data: profile } = await supabase
-      .from("profiles")
-      .select("*")
-      .eq("id", userId)
-      .maybeSingle();
+            const { data: profile } = await supabase
+              .from("profiles")
+              .select("*")
+              .eq("id", userId)
+              .maybeSingle();
 
-    if (profile) {
-      router.replace("/reading-test");
-    } else {
-      router.replace("/onboarding");
-    }
-  }
-};
+            if (profile) {
+              router.replace("/reading-test");
+            } else {
+              router.replace("/onboarding");
+            }
+          }
+        };
 
-checkSession();
+        checkSession();
     const {
       data: { subscription },
     } = supabase.auth.onAuthStateChange(
